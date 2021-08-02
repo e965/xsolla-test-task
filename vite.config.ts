6bace4e4b22a4109f4b8b9bd2dfd07f4494a3c13
Dmitry <git@e965.ru>
2021-08-03 00:22:56 +0500

    initial commit

---
+++ vite.config.ts
@@ -0,0 +1,10 @@
+import { defineConfig } from 'vite';
+
+import reactRefresh from '@vitejs/plugin-react-refresh';
+import reactJsx from 'vite-react-jsx';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+    base: './',
+    plugins: [reactRefresh(), reactJsx()],
+});
