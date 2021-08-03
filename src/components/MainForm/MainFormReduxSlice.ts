2bf2ea72172a7f647fdf1e8c672a36d93c6ba58c
Dmitry <git@e965.ru>
2021-08-03 15:59:14 +0500

    сделал MainFormReduxSlice

---
+++ src/components/MainForm/MainFormReduxSlice.ts
@@ -0,0 +1,14 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const SliceName = 'MainForm';
+
+const MainFormReduxSlice = createSlice({
+    name: SliceName,
+    initialState: {},
+    reducers: {},
+    extraReducers: builder => {
+        //
+    },
+});
+
+export default MainFormReduxSlice.reducer;
--- src/redux/rootReducer.ts
+++ src/redux/rootReducer.ts
@@ -1,6 +1,10 @@
 import { combineReducers } from '@reduxjs/toolkit';
 
-const rootReducer = combineReducers({});
+import mainFormReducer from '../components/MainForm/MainFormReduxSlice';
+
+const rootReducer = combineReducers({
+    mainForm: mainFormReducer,
+});
 
 type RootReducerType = typeof rootReducer;
 export type RootStateType = ReturnType<RootReducerType>;
