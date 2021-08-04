4b494900500a7e493dd4729efc240fcfa3bc863f
Dmitry <git@e965.ru>
2021-08-04 15:33:37 +0500

    для истории

--- src/components/MainForm/logic/highlight-variables.ts
+++ src/components/MainForm/logic/highlight-variables.ts
@@ -1,6 +1,6 @@
 import { useState, useCallback } from 'react';
 
-const usehighlightVariables = (Variables: string[]): [string, (value: string) => void] => {
+const usehighlightVariables = (Variables: string[], replaceAllWrap = false): [string, (value: string) => void] => {
     const [TextAreaContent, setTextareaContentState] = useState('');
 
     const handleTextareaTextChange = useCallback(
@@ -10,8 +10,24 @@ const usehighlightVariables = (Variables: string[]): [string, (value: string) =>
             newContent = newContent.replaceAll('<mark>', '');
             newContent = newContent.replaceAll('</mark>', '');
 
+            newContent = newContent.replace(/<font\sstyle=\\"color:\s(.*)\\">/gm, '');
+            newContent = newContent.replace(/<font\sstyle=\"color:\s(.*)\">/gm, '');
+            newContent = newContent.replaceAll('</font>', '');
+
+            newContent = newContent.replace(/<span\sstyle=\\"color:\s(.*)\\">/gm, '');
+            newContent = newContent.replace(/<span\sstyle=\"color:\s(.*)\">/gm, '');
+            newContent = newContent.replaceAll('</span>', '');
+
             newContent = newContent.replaceAll('<div></div>', '');
             newContent = newContent.replaceAll('<div><br></div>', '<br>');
+
+            if (replaceAllWrap) {
+                newContent = newContent.replaceAll('<div>', '');
+                newContent = newContent.replaceAll('</div>', '');
+                newContent = newContent.replaceAll('<br>', '');
+                newContent = newContent.replaceAll('\r', '');
+                newContent = newContent.replaceAll('\n', '');
+            }
 
             if (newContent.trim() === '<br>') {
                 newContent = '';
@@ -23,7 +39,7 @@ const usehighlightVariables = (Variables: string[]): [string, (value: string) =>
 
             setTextareaContentState(newContent);
         },
-        [Variables]
+        [Variables, replaceAllWrap]
     );
 
     return [TextAreaContent, handleTextareaTextChange];
